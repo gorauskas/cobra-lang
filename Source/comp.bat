de0be64a1ada4d65377cf9c4c1cdf01c098db30c
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-08-02 17:21:50 +0000

    Checkpoint.

--- Source/comp.bat
+++ Source/comp.bat
@@ -1,1 +1,2 @@
-cobra --compile --sst CobraMain.cobra CommandLine.cobra Compiler.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra Node.cobra Module.cobra CobraParser.cobra
+cls
+cobra --compile --sst CobraMain.cobra CommandLine.cobra Compiler.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra Node.cobra NameSpace.cobra Module.cobra CobraParser.cobra
--- Source/test.bat
+++ Source/test.bat
@@ -1,1 +1,1 @@
-cobra --test --sst IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra CobraParser.cobra Node.cobra Module.cobra Compiler.cobra CommandLine.cobra
+cobra --test --sst IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra CobraParser.cobra Node.cobra NameSpace.cobra Module.cobra Compiler.cobra CommandLine.cobra
