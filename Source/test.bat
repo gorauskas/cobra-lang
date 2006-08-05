7dabe4236d603a279e7371ec838fa0a61a357c70
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-08-05 05:27:13 +0000

    mod

--- Source/test.bat
+++ Source/test.bat
@@ -1,1 +1,2 @@
+cls
 cobra --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra CobraParser.cobra Node.cobra NameSpace.cobra Module.cobra Compiler.cobra CommandLine.cobra
