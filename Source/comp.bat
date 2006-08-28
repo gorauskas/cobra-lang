fe4771b694af557667ac33cdc431cfda152250fb
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-08-28 07:01:43 +0000

    mod

--- Source/comp.bat
+++ Source/comp.bat
@@ -1,2 +1,2 @@
 cls
-cobra --ultraedit --compile --sst CobraMain.cobra Utils.cobra CommandLine.cobra Compiler.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra OperatorSpecs.cobra Node.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Types.cobra Expr.cobra Statements.cobra CobraParser.cobra
+cobra --editor=uedit32_FILE/LINE --compile --sst CobraMain.cobra Utils.cobra CommandLine.cobra Compiler.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra OperatorSpecs.cobra Node.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Types.cobra Expr.cobra Statements.cobra CobraParser.cobra
--- Source/test.bat
+++ Source/test.bat
@@ -1,2 +1,2 @@
 cls
-cobra --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra OperatorSpecs.cobra CobraParser.cobra Node.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Types.cobra Expr.cobra Statements.cobra Compiler.cobra CommandLine.cobra
+cobra --editor=uedit32_FILE/LINE --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra OperatorSpecs.cobra CobraParser.cobra Node.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Types.cobra Expr.cobra Statements.cobra Compiler.cobra CommandLine.cobra
