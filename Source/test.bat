1152b14aa4bdea0ffe950649f7f2591ec4a5b0e6
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-08-29 05:40:26 +0000

    mod

--- Source/test.bat
+++ Source/test.bat
@@ -1,2 +1,2 @@
 cls
-cobra --editor=uedit32_FILE/LINE --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra OperatorSpecs.cobra CobraParser.cobra Node.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Types.cobra Expr.cobra Statements.cobra Compiler.cobra CommandLine.cobra
+cobra --editor=uedit32_FILE/LINE --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra OperatorSpecs.cobra CobraParser.cobra Node.cobra Container.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Types.cobra Expr.cobra Statements.cobra Compiler.cobra CommandLine.cobra
