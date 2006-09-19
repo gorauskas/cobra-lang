04b1e943ee0258c8dbdee14ff1b79d0f888ccfb4
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-09-19 00:59:04 +0000

    mod

--- Source/comp.bat
+++ Source/comp.bat
@@ -1,2 +1,2 @@
 cls
-cobra --editor=uedit32_FILE/LINE --compile --sst CobraMain.cobra Utils.cobra CommandLine.cobra Compiler.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra Misc.cobra Enums.cobra OperatorSpecs.cobra Node.cobra Container.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Types.cobra Expr.cobra BinaryOpExpr.cobra Statements.cobra CobraParser.cobra
+cobra --editor=uedit32_FILE/LINE --compile --sst CobraMain.cobra Utils.cobra CommandLine.cobra Compiler.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra Misc.cobra Enums.cobra OperatorSpecs.cobra Node.cobra Container.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Indexer.cobra Types.cobra Expr.cobra BinaryOpExpr.cobra Statements.cobra CobraParser.cobra
--- Source/test.bat
+++ Source/test.bat
@@ -1,2 +1,2 @@
 cls
-cobra --editor=uedit32_FILE/LINE --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra Misc.cobra Enums.cobra OperatorSpecs.cobra CobraParser.cobra Node.cobra Container.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Types.cobra Expr.cobra BinaryOpExpr.cobra Statements.cobra Compiler.cobra CommandLine.cobra
+cobra --editor=uedit32_FILE/LINE --test --sst Utils.cobra IndentedWriter.cobra Tokenizer.cobra CobraTokenizer.cobra Misc.cobra Enums.cobra OperatorSpecs.cobra CobraParser.cobra Node.cobra Container.cobra NameSpace.cobra Module.cobra BoxesAndMembers.cobra Property.cobra Indexer.cobra Types.cobra Expr.cobra BinaryOpExpr.cobra Statements.cobra Compiler.cobra CommandLine.cobra
