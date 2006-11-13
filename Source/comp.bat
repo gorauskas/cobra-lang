5e41a8d5aee3b9ef1dc8d1b0d3b18d068dfdcf43
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-11-13 19:34:32 +0000

    mod

--- Source/comp.bat
+++ Source/comp.bat
@@ -1,3 +1,2 @@
 cls
-rem --editor=uedit32_FILE/LINE --compile --sst
-cobra-new --compile %* cobra Utils CommandLine Compiler IndentedWriter Tokenizer CobraTokenizer Misc Enums OperatorSpecs Node Container NameSpace Module BoxesAndMembers Property Indexer Types Expr BinaryOpExpr Statements CobraParser SharpWriter
+cobra-new --editor=uedit32_FILE/LINE --compile %* cobra Utils CommandLine Compiler IndentedWriter Tokenizer CobraTokenizer Misc Enums OperatorSpecs Node Container NameSpace Module BoxesAndMembers Property Indexer Types Expr BinaryOpExpr Statements CobraParser SharpWriter
