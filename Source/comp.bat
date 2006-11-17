e2c4160c45ffe718f3e327ed01f8523220ab5370
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-11-17 06:51:02 +0000

    mod

--- Source/comp.bat
+++ Source/comp.bat
@@ -1,2 +1,2 @@
 cls
-cobra-new --compile %* cobra Utils CommandLine Compiler IndentedWriter Tokenizer CobraTokenizer Misc Enums OperatorSpecs Node Container NameSpace Module BoxesAndMembers Property Indexer Types Expr BinaryOpExpr Statements CobraParser SharpWriter
+cobra-new --compile %* -files=cobra-files.text
