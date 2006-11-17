1237e40423bf150c95f8b81b0a2312eebc6afd79
echuck <echuck@30746285-aa12-4141-907f-d1d40c2173e5>
2006-11-17 05:59:30 +0000

    mod

--- Source/CobraLang.cs
+++ Source/CobraLang.cs
@@ -12,13 +12,13 @@ static public class CobraCore {
 
 	static public Version Version {
 		get {
-			return new Version(0, 1, 4);
+			return new Version(0, 2, 0);
 		}
 	}
 
 	static public int ReleaseNum {
 		get {
-			return 14;  // increment by exactly one with each release, no matter how big or small
+			return 15;  // increment by exactly one with each release, no matter how big or small
 		}
 	}
 
