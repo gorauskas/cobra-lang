1a9d1539c0cbe4260e47582dd72c2248fafc88d2
Chuck.Esterbrook <Chuck.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2008-10-20 01:00:07 +0000

    Add a note to help Windows Vista users.

--- Source/install-from-workspace.bat
+++ Source/install-from-workspace.bat
@@ -1,2 +1,7 @@
 @cls
+@echo If you are on Windows Vista and you get an error message then
+@echo you may need to run with elevated privileges, such as with an
+@echo admin account or power user account.
+@echo.
+@echo Compiling installation program...
 @Snapshot\cobra -debug -ert:yes InstallFromWorkspace.cobra
