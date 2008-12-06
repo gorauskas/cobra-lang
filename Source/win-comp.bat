43d76e34d3a11ed789f0eb2ec9278ca43e436e1b
Chuck.Esterbrook <Chuck.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2008-12-06 07:01:20 +0000

    Fix.

--- Source/win-comp.bat
+++ Source/win-comp.bat
@@ -1,1 +1,1 @@
-Snapshot\cobra.exe -sharp-args:"/out:cobra-win.exe" -compile -color -debug -timeit -t:winexe -r:System.Windows.Forms -r:System.Drawing %* -files:files-to-compile-win.text
+Snapshot\cobra.exe -sharp-args:"/out:cobra-win.exe" -compile -color -debug -timeit -t:winexe -ref:System.Windows.Forms -ref:System.Drawing %* -files:files-to-compile-win.text
