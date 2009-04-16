9d4f8e0e3a32620367e7feb2d90becd3574895c6
Chuck.Esterbrook <Chuck.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2009-04-16 20:47:32 +0000

    Refactor files-to-compile.text and files-to-compile-win.text to eliminate duplication.

--- Source/comp.bat
+++ Source/comp.bat
@@ -1,1 +1,1 @@
-Snapshot\cobra.exe -compile -color -debug -ert:yes -timeit %* -files:files-to-compile.text
+Snapshot\cobra.exe -compile -color -debug -ert:yes -timeit %* cobra.cobra -files:files-to-compile.text
