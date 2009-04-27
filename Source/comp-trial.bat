7f12cdd4fe1a919ac7aa3e863a8001244ebe1213
Chuck.Esterbrook <Chuck.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2009-04-27 02:56:41 +0000

    Fix comp-trial scripts from earlier check-in to refactor files-to-compile[-win].text files.

--- Source/comp-trial.bat
+++ Source/comp-trial.bat
@@ -1,1 +1,1 @@
-cobra-trial.exe -compile -color -debug -ert:yes -timeit %* -files:files-to-compile.text
+cobra-trial.exe -compile -color -debug -ert:yes -timeit %* cobra.cobra -files:files-to-compile.text
