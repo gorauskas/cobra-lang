8c8227e94b5747f8a28c9c39d2fd1d5a9b78593d
Charles.Esterbrook <Charles.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2010-12-06 01:52:20 +0000

    Fixed: Using a `uint?` expression for truthfulness, such as in `if` or `assert`, generates false compilation errors.

--- Source/Cobra.Lang/Native.cs
+++ Source/Cobra.Lang/Native.cs
@@ -388,6 +388,30 @@ static public class CobraImp {
 		return i!=null && i.Value!=0;
 	}
 
+	static public bool IsTrue(uint i) {
+		return i!=0;
+	}
+
+	static public bool IsTrue(uint? i) {
+		return i!=null && i.Value!=0;
+	}
+
+	static public bool IsTrue(long i) {
+		return i!=0;
+	}
+
+	static public bool IsTrue(long? i) {
+		return i!=null && i.Value!=0;
+	}
+
+	static public bool IsTrue(ulong i) {
+		return i!=0;
+	}
+
+	static public bool IsTrue(ulong? i) {
+		return i!=null && i.Value!=0;
+	}
+
 	static public bool IsTrue(decimal d) {
 		return d!=0;
 	}
