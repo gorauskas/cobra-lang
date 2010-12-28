e0ca39bcd10b508a69ab6442d18a80e9b61bb01c
Charles.Esterbrook <Charles.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2010-12-28 20:55:36 +0000

    Improvements to the installer including:  * Detecting if the user is not root, which is required.  * New -skip-std-lib command line option. credit:todd.a

--- Source/install-from-workspace.bat
+++ Source/install-from-workspace.bat
@@ -1,7 +1,4 @@
 @cls
-@echo If you are on Windows Vista and you get an error message then
-@echo you may need to run with elevated privileges, such as with an
-@echo admin account or power user account.
 @echo.
 @echo Compiling installation program...
 @Snapshot\cobra.exe -debug -ert:yes InstallFromWorkspace.cobra -- %*
