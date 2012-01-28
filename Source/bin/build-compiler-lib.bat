b708d74309649f4c8bbe2e37ad2fe7ec16c5c9c9
Charles.Esterbrook <Charles.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2012-01-28 18:39:10 +0000

    Fixed: build-compiler-lib.bat is missing the proper path to build.bat reported-by:melger

--- Source/bin/build-compiler-lib.bat
+++ Source/bin/build-compiler-lib.bat
@@ -1,2 +1,2 @@
-build -t:lib -namespace:Cobra.Lang.Compiler -out:Cobra.Lang.Compiler.dll %*
+call bin\build -t:lib -namespace:Cobra.Lang.Compiler -out:Cobra.Lang.Compiler.dll %*
 dir Cobra.Lang.Compiler.*
