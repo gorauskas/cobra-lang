3337d63df6c0c615433b4cc467bbee7d2bebd0d6
Charles.Esterbrook <Charles.Esterbrook@30746285-aa12-4141-907f-d1d40c2173e5>
2013-05-14 00:49:49 +0000

    Testify: List cobra.exe and Cobra.Core.dll, including their creation dates, along with the current date.

--- Source/bin/testify.bat
+++ Source/bin/testify.bat
@@ -1,4 +1,15 @@
-@cls
+@echo off
+cls
+
+echo Cobra Testify
+echo.
+date /t
+time /t
+@rem dir cobra.exe Cobra.Core.dll
+dir cobra.exe | findstr cobra.exe
+dir Cobra.Core.dll | findstr Cobra.Core.dll
+echo.
+
 type NUL > r-testify-failures.text
 cobra -testify %*
 @rem e r-testify
